2ca355198c55a1f06d87d6995b0a94154b4c3d9c
bareno <barenojuan619@gmail.com>
2025-07-18 12:30:11 -0500

    Feat: :fire: First class of Pedro

---
+++ Dia1/scripts/script.js
@@ -0,0 +1,61 @@
+// DIA 1
+
+
+// ¡¡¡PRIMER COMANDO!!!
+
+console.log("Hola Mundo!"); // Así imprimimos, con console.log
+variable1 = 3.14; // De esta manera creamos una variable y le damos un valor predefinido
+
+console.log(variable1) // De esta manera imprimimos el valor que hay en una variable
+
+console.log(typeof variable1) // typeof <- Muestra el tipo de dato que contiene la variable
+
+
+
+// ¡¡¡CONCATENACIÓN!!!
+
+// Creamos dos variables con un valor cada una.
+palabra1 = "Campus"; 
+palabra2 = "Lands";
+
+// Creamos otra variable y le añadimos los valores de las anteriores variables, concatenando las variables. 
+palabra3 = palabra1+palabra2;
+
+// Imprimimos los valores de cada varible
+console.log(palabra1);
+console.log(palabra2);
+console.log(palabra3);
+
+
+
+// ¡¡¡BOOLEANOS!!!
+booleanito1 = false;
+booleanito2 = false;
+console.log(typeof booleanito1);
+
+
+
+// ¡¡¡CONDICIONALES!!!
+if(booleanito1 == true) {
+    console.log("Es verdadero!!");
+}else{
+    console.log("No es verdadero :(");
+}
+
+
+
+// ¡¡¡BUCLES!!!
+
+// BUCLE FOR
+for(i=0;i<4;i=i+1) // i vale 0; mientras que i sea menor a 4; sumarle 1 al iterador i.
+{
+    console.log(i,"Wenas");
+}
+
+// BUCLE WHILE
+holi = true;
+while(holi == true) {
+    console.log("wenas");
+    
+}
+// Desarrollado por : Juan Pablo Bareño Sierra - T.I 1.098.677.321
