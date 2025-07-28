48f22f6d636afbaa12206919f827d4545e6fab5b
Juan Bareño <barenojuan619@gmail.com>
2025-07-28 16:07:27 -0500

    Feat: :see_no_evil: Learning about APIS

--- Dia6/script.js
+++ Dia6/script.js
@@ -1,20 +1,5 @@
-const objetoRick = new XMLHttpRequest();
-objetoRick.open("GET", "https://rickandmortyapi.com/api");
+// Creo una especie de "Mensajero" o "intermediario" entre mi app o página, que se comunique con la API o servidor que contiene la información"
+const peticionAPI = new XMLHttpRequest();
 
+peticionAPI.open('GET',' https://rickandmortyapi.com/api')
 
-objetoRick.onload = function() {
-    if (objetoRick.status >= 200 && objetoRick.status > 300) {
-        const rickData = JSON.parse(objetoRick.response);
-
-        console.log(rickData);
-    }
-    else {
-        console.log("Error en la petición.");
-    }
-}
-
-objetoRick.onerror = function() {
-    console.log("Error de red.");
-}
-
-objetoRick.send();
