2050f04916b9ebcff9db2057a4c3cd50cf7825ce
Juan Bareño <barenojuan619@gmail.com>
2025-07-31 17:58:20 -0500

    Feat: :tada: I finish my Pokemon Atari

--- Dia9/TAREA/script.js
+++ Dia9/TAREA/script.js
@@ -1,30 +1,82 @@
-function buscarPokemon() {
+function buscarPokemon(pokemonValue) {
 
-    // Con esto consigo manipular el input que está en el HTML (obtengo lo que se ingrese ahí)
-    const inputPokemon = document.getElementById("inputAtari")
+    const pokemon = String(pokemonValue).toLowerCase();
 
-    // Creo el mensajerito
-    const xhr = new XMLHttpRequest;
+    // Guardo la URL dentro de una variable
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    
 
-    const url = `https://pokeapi.co/api/v2/pokemon/?:id=`
+    // Inicializo esto para poder iniciar una comunicación con la API
+    const xhr = new XMLHttpRequest();
 
-    xhr.open("GET", url, true)
+    xhr.open("GET", url, true);
 
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            try {
-                const daticos =  JSON.parse(xhr.responseText);
 
-                console.log(daticos)
-            }
-            catch {
-                console.log("Hubo un errorsini")
-            }
+            const daticos = JSON.parse(xhr.responseText);
+
+            // Obtengo los elementos que necesito de mi HTML
+            const pokemonImagen = document.querySelector('.pokemon');
+            const pokemonNombre = document.querySelector('.nombrePokemon span:first-child');
+            const pokemonId = document.querySelector('.nombrePokemon span:last-child');
+
+
+            // Creamos un elemento de tipo "img", para poder guardar la foto del pokemon
+            const imgPokemon = document.createElement('img');
+            imgPokemon.src = daticos.sprites.front_default;
+
+
+            // Limpio el div para poder agregar una nueva imagen
+            pokemonImagen.innerHTML = "";
+            pokemonImagen.appendChild(imgPokemon);
+
+            // Limpio el texto del nombre yu el id también
+            pokemonNombre.innerText = daticos.name;
+            pokemonId.innerText = `- ${daticos.id}`
+
+            pokeID = daticos.id;
+            
         }
     }
 
     xhr.send();
 
+
 }
 
-buscarPokemon();
+
+
+
+const input = document.getElementById('inputAtari');
+
+input.addEventListener('keydown', function (evento) {
+
+    // Verifico que la tecla presionada sea enter, para llamar a la función
+    if (evento.key === 'Enter') {
+        buscarPokemon(input.value);
+    }
+});
+
+// obtengo el botón izquierdo de mi html
+const prevBtn = document.querySelector('.bPrev');
+
+let pokeID = 1;
+
+prevBtn.addEventListener('click', function () {
+    if (pokeID > 1) {
+        // Le resto 1 al valor del pokeID
+        pokeID--;
+        buscarPokemon(pokeID);
+    }
+})
+
+
+// obtengo el botón derecho de mi html
+const nextBtn = document.querySelector('.bNext');
+
+nextBtn.addEventListener('click', function () {
+    // le sumo 1 al valor del pokeID
+    pokeID++;
+    buscarPokemon(pokeID);
+})
