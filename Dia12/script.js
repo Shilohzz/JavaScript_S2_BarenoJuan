0ece4244b9e857bc669ec414b25b2d7319d35656
bareno <barenojuan619@gmail.com>
2025-08-04 12:06:34 -0500

    Feat: :sparkles: Class Day 12

---
+++ Dia12/script.js
@@ -0,0 +1,5 @@
+document.getElementById('btnColor').addEventListener('click',()=>{
+    console.log("Wenas");
+    const cuadro = document.getElementById('cuadro');
+    cuadro.classList.toggle('cambio')
+});
